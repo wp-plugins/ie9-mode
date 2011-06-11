fc5acb81045024923ba5ebb301f143606be3e794
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-11 08:53:47 +0000

    readme fix
    git-svn-id: https://plugins.svn.wordpress.org/ie9-mode/trunk@395894 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ie9-mode.php
+++ ie9-mode.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Internet Explorer 9 compatible
-Plugin URI: http://www.ramoonus.nl
+Plugin URI: http://www.ramoonus.nl/wordpress/ie9-mode/
 Description: Forces Internet Explorer 9 and higher to render the page as version 9.
 Version: 1.0.0
 Author: Ramoonus.nl
-Author URI: http://www.ramoonus.nl
+Author URI: http://www.ramoonus.nl/
 */
 
 function rw_ie9() {
